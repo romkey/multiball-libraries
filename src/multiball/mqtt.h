3f4e7fae7abfd9a165ad94e590ea72d6e04b55ab
John Romkey <58883+romkey@users.noreply.github.com>
2019-10-26 08:17:02 -0700

    declare mqtt_persist() and mqtt_restore()

--- src/multiball/mqtt.h
+++ src/multiball/mqtt.h
@@ -14,3 +14,6 @@ void mqtt_subscribe(const char* topic);
 
 void mqtt_publish(const char* topic, const char* payload, bool retain = false);
 bool mqtt_is_connected();
+
+void mqtt_persist();
+boolean mqtt_restore();
